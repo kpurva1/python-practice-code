f044298b42c98d617fde390edd6d97efa3d075f3
kpurva1 <kpurva1@umbc.edu>
2017-04-11 11:54:31 -0400

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -1,5 +1,6 @@
 
 #Given an array of integers, return indices of the two numbers such that they add up to a specific target. Given nums = [2, 7, 11, 15], target = 9,Because nums[0] + nums[1] = 2 + 7 = 9,return [0, 1].
+#space complexity O(1), time complexity O(n2)
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -11,5 +12,22 @@ class Solution(object):
             for y in range (x+1,len(nums)):
                 if (nums[x] + nums[y]) == target:
                     return [x,y]
-                    
+ 
+
+
+#another solution using dictionary
+#space complexity O(n), time complexity O(n)
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hm = dict()
+        for i in range (len(nums)):
+            if nums[i] in hm:
+                return (hm[nums[i]],i)
+            hm[target - nums[i]] = i;
+        
             
